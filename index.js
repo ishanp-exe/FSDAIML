b9851eac7d1d8fa2a00ae33bf48d2b317a6e209b
ishanp-exe <ishanpandey1610@gmail.com>
2026-07-24 10:13:34 +0530

    updated

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 function sum(a,b){
     return a+b;
 }
-const result = sum(20,50);
-console.log("Sum " + result);
+// const result = sum(20,50);
+// console.log("Sum " + result);
+
+
+// function SumWithMsg(clbk,msg){
+//     const result = clbk(20,40);
+//     console.log(msg + ":" + result)
+// }
+// SumWithMsg(sum, "Hey using calculation with JS")
+
+
+function login(msg,err){
+    if(error){
+        console.log("Error is " + error)
+    }else{
+        console.log ("Welcome "+ msg);
+    }
+}
+function loginVerification(username,password,clbk){
+    if (username == "Ishan1610" && password == "XYZ"){
+        clbk("Success", null);
+    }else{
+        clbk(null, "Username or password is incorrect")
+    }
+}
+loginVerification("Ishan1610", "XYZ", login)
